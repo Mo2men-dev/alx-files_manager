9aa2f4521eb5d75d8320171de6dddc01e2a4e754
Mo2men-dev <momensameh5@gmail.com>
2024-09-27 10:39:11 +0300

    Add MongoDB client utility for database operations

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+class DBClient {
+    constructor() {
+        MongoClient.connect(URL, { useUnifiedTopology: true }, (err, client) => {
+            if (!err) {
+                this.db = client.db(DB_DATABASE);
+            } else {
+                this.db = false;
+            }
+        });
+
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+    }
+
+    isAlive() {
+        return !!this.db;
+    }
+
+    async nbUsers() {
+        return this.users.countDocuments();
+    }
+
+    async nbFiles() {
+        return this.files.countDocuments();
+    }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
